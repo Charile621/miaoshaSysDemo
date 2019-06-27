fe1590595ba078cd1ee27a3d6fddc0f2f09979fb
charile_Lu <www.1072571694@qq.com>
2019-06-27 14:48:08 +0800

    实现了短信验证和用户注册功能

--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.miaoshaproject.controller;
 
+import com.alibaba.druid.util.StringUtils;
 import com.miaoshaproject.controller.viewobject.UserVO;
 import com.miaoshaproject.error.BusinessException;
 import com.miaoshaproject.error.EmBusinessError;
@@ -10,14 +11,86 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import sun.misc.BASE64Encoder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 
 @Controller("user")
 @RequestMapping("/user")
+@CrossOrigin(allowCredentials = "true",allowedHeaders = "*")
 public class UserController extends BaseController{
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    //用户注册接口
+    @RequestMapping(value = "/register",method = {RequestMethod.POST},consumes={CONTENT_TYPE_FORMED})
+    @ResponseBody
+    public CommonReturnType register(@RequestParam(name="telphone")String telphone,
+                                     @RequestParam(name="otpCode")String otpCode,
+                                     @RequestParam(name="name")String name,
+                                     @RequestParam(name="gender")String gender,
+                                     @RequestParam(name="age")Integer age,
+                                     @RequestParam(name="password")String password) throws BusinessException, UnsupportedEncodingException, NoSuchAlgorithmException {
+        //验证手机号和对应的otpCode相符合
+        String inSessionOtpCode = (String)this.httpServletRequest.getSession().getAttribute(telphone);
+        if(!StringUtils.equals(otpCode,inSessionOtpCode))
+        {
+            throw new BusinessException(EmBusinessError.PARAMETER_VAILDATION_ERROR,"短信验证码不符合");
+        }
+
+
+        //用户的注册流程
+        UserModel userModel = new UserModel();
+        userModel.setName(name);
+        userModel.setGender(Integer.valueOf(gender));
+        userModel.setAge(age);
+        userModel.setTelphone(telphone);
+        userModel.setRegisterMode("byphone");
+        userModel.setEncrptPassword(this.EncodeByMd5(password));
+
+        userService.register(userModel);
+        return CommonReturnType.create(null);
+    }
+
+    public String EncodeByMd5(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        //确定计算方法
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        BASE64Encoder base64Encoder = new BASE64Encoder();
+
+        //加密字符串
+        String newstr = base64Encoder.encode(md5.digest(str.getBytes("utf-8")));
+        return newstr;
+    }
+
+
+    //用户获取otp短信接口
+    @RequestMapping(value = "/getotp",method = {RequestMethod.POST},consumes={CONTENT_TYPE_FORMED})
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name="telphone")String telphone)
+    {
+        //需按照一定的规则生成otp验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt += 10000;
+        String otpCode = String.valueOf(randomInt);
+
+        //将otp验证码通对应的手机关联,使用HttpSession的方式绑定他的手机号与otpCode
+        httpServletRequest.getSession().setAttribute(telphone,otpCode);
+
+        //将otp验证码通过短信通道发送给用户
+        System.out.println("telphone="+telphone+"&otpCode="+otpCode);
+
+        return CommonReturnType.create(null);
+    }
 
     @RequestMapping("/get")
     @ResponseBody
