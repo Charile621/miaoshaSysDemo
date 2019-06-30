0519c77944aede1513b379b8d00a7ef5f3624315
charile_Lu <www.1072571694@qq.com>
2019-06-30 13:24:56 +0800

    实现了商品模型的设计和相关数据库表的创建，以及使用mybatis-genertor生成相关的映射

---
+++ src/main/java/com/miaoshaproject/service/impl/ItemServiceImpl.java
@@ -0,0 +1,86 @@
+package com.miaoshaproject.service.impl;
+
+import com.miaoshaproject.dao.ItemDOMapper;
+import com.miaoshaproject.dao.ItemStockDOMapper;
+import com.miaoshaproject.dataobject.ItemDO;
+import com.miaoshaproject.dataobject.ItemStockDO;
+import com.miaoshaproject.error.BusinessException;
+import com.miaoshaproject.error.EmBusinessError;
+import com.miaoshaproject.service.ItemService;
+import com.miaoshaproject.service.model.ItemModel;
+import com.miaoshaproject.validator.ValidationResult;
+import com.miaoshaproject.validator.ValidatorImpl;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    private ValidatorImpl validator;
+
+    @Autowired
+    private ItemDOMapper itemDOMapper;
+
+    @Autowired
+    private ItemStockDOMapper itemStockDOMapper;
+
+    private ItemDO convertItemDOFromItemModel(ItemModel itemModel)
+    {
+        if(itemModel==null)
+        {
+            return null;
+        }
+        ItemDO itemDO = new ItemDO();
+        BeanUtils.copyProperties(itemModel,itemDO);
+        itemDO.setPrice(itemModel.getPrice().doubleValue());
+        return itemDO;
+    }
+
+    private ItemStockDO convertItemStockDOFromItemModel(ItemModel itemModel)
+    {
+        if(itemModel==null)
+        {
+            return null;
+        }
+        ItemStockDO itemStockDO = new ItemStockDO();
+        itemStockDO.setItemId(itemModel.getId());
+        itemStockDO.setStock(itemModel.getStock());
+        return itemStockDO;
+    }
+
+    @Override
+    @Transactional
+    public ItemModel createItem(ItemModel itemModel) throws BusinessException {
+        //校验入参
+        ValidationResult validationResult = validator.validate(itemModel);
+        if(validationResult.isHasErrors())
+        {
+            throw new BusinessException(EmBusinessError.PARAMETER_VAILDATION_ERROR,validationResult.getErrMsg());
+        }
+        //转化itemmodel->dataobject
+        ItemDO itemDO = convertItemDOFromItemModel(itemModel);
+
+        //写入数据库
+        itemDOMapper.insertSelective(itemDO);
+
+        //返回创建完成的对象
+
+
+        return null;
+    }
+
+    @Override
+    public List<ItemModel> listItem() {
+        return null;
+    }
+
+    @Override
+    public ItemModel getItemById(Integer id) {
+        return null;
+    }
+}
